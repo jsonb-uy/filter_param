44d96cb289842ed88be82bed317d720c11e9b190
jsonb-uy <uy.json.dev@gmail.com>
2023-07-12 15:38:14 +0800

    Refactor transpiler

--- lib/filter_param/filter/transpiler.rb
+++ lib/filter_param/filter/transpiler.rb
@@ -10,23 +10,26 @@ module FilterParam
       def transpile!(expression)
         return nil if expression.blank?
 
-        parse_tree = Parser.new.parse(expression, reporter: Parslet::ErrorReporter::Deepest.new)
-        ast = AST::Transformer.new.apply(parse_tree, definition: definition)
-
-        permission_checker.visit_node(ast)
-                          .then { |ast| backend.visit_node(ast) }
+        expression_to_ast!(expression).then { |ast| check_field_permissions!(ast) }
+                                      .then { |ast| transpile_to_sql!(ast) }
       end
 
       private
 
       attr_reader :definition
 
-      def permission_checker
-        AST::FieldPermissionChecker.new(definition)
+      def expression_to_ast!(expression)
+        parse_tree = Parser.new.parse(expression, reporter: Parslet::ErrorReporter::Deepest.new)
+
+        AST::Transformer.new.apply(parse_tree, definition: definition)
       end
 
-      def backend
-        Backend::Postgresql.new(definition)
+      def check_field_permissions!(ast)
+        AST::FieldPermissionChecker.new(definition).visit_node(ast)
+      end
+
+      def transpile_to_sql!(ast)
+        Backend::Postgresql.new(definition).visit_node(ast)
       end
     end
   end
